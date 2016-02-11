db59343c53f6c984db97d30377ae1f588538df12
Ahmed Mansour <eviltoys@zoho.com>
2016-02-11 19:05:37 +0000

    Turn on/off digital pins instead of using tone that don't work on multiple pin in same time

--- vibrate/vibrate.ino
+++ vibrate/vibrate.ino
@@ -1,23 +1,26 @@
 /*
+Copyright Ahmed Mansour 2016
 
-
- Plays tones on digital pins 6 (P1_4)
-
-
- http://arduino.cc/en/Tutorial/Tone4
+ Turn on/off digital pins 6 (P1_4)
+ Turn on/off digital pins 7 (P1_5)
 
  */
-//int frequency = 31000; // 31kHz Ultrasound
-int frequency = 9000;
-void setup() {
 
+void setup() {
+  // initialize the digital pin as an output.
+  pinMode(6, OUTPUT); 
+  // initialize the digital pin as an output.
+  pinMode(7, OUTPUT); 
 }
 
 void loop() {
 
-  // play a note on pin 6 for 200 ms:
-  tone(6, frequency, 200);
+  digitalWrite(6, HIGH);
+  digitalWrite(7, HIGH);
   delay(200);
-
+  
+  digitalWrite(6, LOW); 
+  digitalWrite(7, LOW);
+  delay(200);
 
 }
